dd5b01f6097003d88f2e6fe76d720116ddf9d6eb
YvanPB21 <yvanpb.2000.yp@gmail.com>
2021-08-20 22:40:01 -0500

    Base Project

---
+++ src/main/java/com/upc/hydroti/HydroTiApplication.java
@@ -0,0 +1,13 @@
+package com.upc.hydroti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroTiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydroTiApplication.class, args);
+    }
+
+}
